425d07d3e349c43ccfe75c0803ab5e3a6daea5a1
Enzo Yuta Nagasava <enzonagasava@gmail.com>
2025-02-15 02:51:48 -0300

    finalizando secao sobre mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo/index.tsx'
 import Paragrafo from '../../components/Paragrafo/index.tsx'
+import { GithubSecao } from './styles.ts'
 
 const Sobre = () => (
   <section>
@@ -9,6 +10,10 @@ const Sobre = () => (
       assumenda iusto, odio pariatur neque provident dicta sequi. A provident
       dolor facilis est aspernatur sunt eveniet dolores deserunt totam cum.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=enzonagasava&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=enzonagasava&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
